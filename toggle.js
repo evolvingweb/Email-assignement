91baf1ac4104d876fef5acdac115e7ea4a636d65
Mac_Weber <Mac_Weber@789986.no-reply.drupal.org>
2016-03-14 04:38:13 -0300

    Fix CSS and autocomplete. Remove Google geolocation.

--- toggle.js
+++ toggle.js
@@ -6,54 +6,24 @@
  */
 
 $(document).ready(function() {
+  // List of available cities.
+  // Remote data is not working in Firefox. Google library is an lighter option
+  // than JQuery UI: https://jsfiddle.net/cbwd42yv/
+  var availableCities = [
+    "Montreal, Canada",
+    "Ottawa, Canada",
+    "Toronto, Canada",
+    "New York, USA"
+  ];
+
   // Make sure the field will be displayed on page reload if there is input.
   toggleCity();
 
   // Autocomplete for 'city' field.
   $( "#mce-CITY" ).autocomplete({
-    source: "data/cities.json",
-    dataType: "jsonp",
+    source: availableCities,
     minLength: 2
   });
-
-  // Geolocation using HTML5 and reversed by Google.
-  var locationInput = $("input[name='CITY']");
-  var defaultLocationText = "Montreal, Canada";
-  if (window.navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        var lat 	 = position.coords.latitude,
-          lng 	 = position.coords.longitude,
-          latlng 	 = new google.maps.LatLng(lat, lng),
-          geocoder = new google.maps.Geocoder();
-        geocoder.geocode({'latLng': latlng}, function(results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-            if (results[1]) {
-              for (var i = 0; i < results.length; i++) {
-                if (results[i].types[0] === "locality") {
-                  var city = results[i].address_components[0].short_name;
-                  var state = results[i].address_components[2].short_name;
-                  locationInput.val(city + ", " + state);
-                  // Stops at the first result.
-                  break;
-                }
-              }
-            }
-            else {
-              console.log("No reverse geocode results.");
-              locationInput.val(defaultLocationText);
-            }
-          }
-          else {
-            console.log("Geocoder failed: " + status);
-            locationInput.val(defaultLocationText);
-          }
-        });
-      },
-      function() {
-        console.log("Geolocation not available.");
-        locationInput.val(defaultLocationText);
-      });
-  }
 
 });
 
@@ -62,12 +32,12 @@ $(document).ready(function() {
  */
 function toggleCity() {
   var hasInput = document.getElementById("mce-EMAIL").value.length;
-  var cityField = $(".mc-field-group-city");
+  var cityField = $("#mc-field-group-city");
 
   if (hasInput) {
     cityField.show("slow");
   }
   else {
-    cityField.hide("slow");
+    cityField.hide();
   }
 }
